1170e9f6c15800bd1e07fcba2cf91b7f7d02831a
Titov German <german.titov@icl-services.com>
2025-03-01 02:40:42 +0300

    second

--- Restapp/drf/cars/serializers.py
+++ Restapp/drf/cars/serializers.py
@@ -5,28 +5,36 @@ from .models import Car
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
-class CarsModel:
-    def __init__(self, title, content):
-        self.title = title,
-        self.content = content
+# class CarsModel:
+#     def __init__(self, title, content):
+#         self.title = title,
+#         self.content = content
 
 class CarSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length = 255)
-    contenet = serializers.CharField()
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only = True)
+    time_update = serializers.DateTimeField(read_only = True)
+    is_published = serializers.BooleanField(default=True)
+    cat = serializers.IntegerField()
 
-def encode():
-    model = CarsModel('bobslav', 'content: mashina')
-    model_sr = CarSerializer(model)
-    print(model_sr.data, type(model_sr.data), sep = '\n')
-    json = JSONRenderer().render(model_sr.data)
-    print(json)
+# class CarSerializer(serializers.ModelSerializer):
+#     title = serializers.CharField(max_length = 255)
+#     contenet = serializers.CharField()
 
-def decode():
-    stream = io.BytesIO(b'{"title": "sobakoslava", "content": "content: zachem"')
-    data = JSONParser().parse(stream)
-    serializer = CarSerializer(data = data)
-    serializer.is_valid()
-    print(serializer.validated_data)
+# def encode():
+#     model = CarsModel('bobslav', 'content: mashina')
+#     model_sr = CarSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep = '\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title": "sobakoslava", "content": "content: zachem"')
+#     data = JSONParser().parse(stream)
+#     serializer = CarSerializer(data = data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
 
 # class CarSerializer(serializers.ModelSerializer):
 #     class Meta:
--- Restapp/drf/cars/views.py
+++ Restapp/drf/cars/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django.forms.models import model_to_dict
 from .models import Car
 from .serializers import CarSerializer
+from rest_framework.response import Response
+
+# class CarsAPIView(generics.ListAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
 
 class CarsAPIView(generics.ListAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
+    def get(self, request):
+        c = Car.object.all()
+        return Response({'posts': CarSerializer(c, many = True).data})
+
+    def post(self, request):
+        serializer = CarSerializer(data = request.data)
+        serializer.is_valid(raise_execption = True)
+
+        post_new = Car.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+
+        return Response({'post': model_to_dict(post_new).data})
