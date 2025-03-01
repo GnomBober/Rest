ae4a094b736dd14b0118581a2c150ec3db625bab
Titov German <german.titov@icl-services.com>
2025-03-01 04:18:22 +0300

    cm

--- Restapp/drf/cars/views.py
+++ Restapp/drf/cars/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from django.forms.models import model_to_dict
-
+from rest_framework.decorators import action
 from .apps import CarsConfig
-from .models import Car
+from .models import Car, Category
 from .serializers import CarSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 class CarsViewSet(viewsets.ModelViewSet):
-    queryset = Car.objects.all()
+    # queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+
+        if not pk:
+            return Car.odjects.all()[:3]
+
+        return Car.objects.filter(pk = pk)
+
+    @action(methods = ['get'], detail = False)
+    def category(self, request, pk = None):
+        cats = Category.objects.get(pk = pk)
+        return Response({'cats': cats.name})
 
 # class CarsAPIView(generics.ListAPIView):
 #     queryset = Car.objects.all()
--- Restapp/drf/drf/urls.py
+++ Restapp/drf/drf/urls.py
@@ -19,8 +19,24 @@ from django.urls import path, include
 from cars.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'cars', CarsViewSet)
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'})
+    ]
+
+router = MyCustomRouter()
+router.register(r'cars', CarsViewSet, basename = 'cars')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
